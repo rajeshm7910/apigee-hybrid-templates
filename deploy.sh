dd3c6aac3460c7f50f5e7404f0a444efab60a5d2
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro3.roam.corp.google.com>
2020-04-04 13:35:32 -0700

    apigee 1.2.0 version

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-project_id=$(gcloud config get-value project)
-project_number=$(gcloud projects describe $project_id | grep projectNumber |  cut -d ':' -f2 | tr -d "'" | awk '{print $1}')
+add_roles() {
 
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.clusterAdmin --quiet
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.admin --quiet
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/resourcemanager.projectIamAdmin --quiet
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/container.admin --quiet
+	project_id=$(gcloud config get-value project)
+	project_number=$(gcloud projects describe $project_id | grep projectNumber |  cut -d ':' -f2 | tr -d "'" | awk '{print $1}')
 
-gcloud services enable container.googleapis.com 
-gcloud services enable iam.googleapis.com
-gcloud services enable cloudresourcemanager.googleapis.com
-gcloud services enable runtimeconfig.googleapis.com
-gcloud services enable sourcerepo.googleapis.com
-gcloud services enable logging.googleapis.com
-gcloud services enable monitoring.googleapis.com
-gcloud services enable apigee.googleapis.com
-gcloud services enable apigeeconnect.googleapis.com
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.clusterAdmin --quiet
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.admin --quiet
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/resourcemanager.projectIamAdmin --quiet
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/container.admin --quiet
+}
 
+enable_services() {
+	gcloud services enable container.googleapis.com 
+	gcloud services enable iam.googleapis.com
+	gcloud services enable cloudresourcemanager.googleapis.com
+	gcloud services enable runtimeconfig.googleapis.com
+	gcloud services enable sourcerepo.googleapis.com
+	gcloud services enable logging.googleapis.com
+	gcloud services enable monitoring.googleapis.com
+	gcloud services enable apigee.googleapis.com
+	gcloud services enable apigeeconnect.googleapis.com
+}
 
-sed  "s/{{org}}/$project_id/g" apigee-cluster.yaml > apigee-cluster-main.yaml
-sed -i.bak "s/{{project_id}}/$project_id/g" apigee-cluster-main.yaml
-uniqueId=$(echo $((RANDOM%10000)))
-sed -i.bak "s/{{uniqueId}}/$uniqueId/g" apigee-cluster-main.yaml
-gcloud deployment-manager deployments create $1 --config apigee-cluster-main.yaml
-rm -fr apigee-cluster-main.yaml*
+deploy() {
+	gcloud deployment-manager deployments create $1 --config apigee-cluster.yaml
+}
+
+add_roles
+enable_services
+deploy "$@"
--- update.sh
+++ update.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-project_id=$(gcloud config get-value project)
-project_number=$(gcloud projects describe $project_id | grep projectNumber |  cut -d ':' -f2 | tr -d "'" | awk '{print $1}')
+#!/bin/bash
 
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.clusterAdmin --quiet
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.admin --quiet
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/resourcemanager.projectIamAdmin --quiet
-gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/container.admin --quiet
+add_roles() {
+	
+	project_id=$(gcloud config get-value project)
+	project_number=$(gcloud projects describe $project_id | grep projectNumber |  cut -d ':' -f2 | tr -d "'" | awk '{print $1}')
 
-sed  "s/{{org}}/$project_id/g" apigee-cluster.yaml > apigee-cluster-main.yaml
-uniqueId=$(gcloud beta runtime-config configs variables get-value  --config-name $1-apigee-config uniqueId)
-sed -i.bak "s/{{uniqueId}}/$uniqueId/g" apigee-cluster-main.yaml
-gcloud deployment-manager deployments update $1 --config apigee-cluster-main.yaml
-rm -fr apigee-cluster-main.yaml*
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.clusterAdmin --quiet
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number-compute@developer.gserviceaccount.com --role roles/container.admin --quiet
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/resourcemanager.projectIamAdmin --quiet
+	gcloud projects add-iam-policy-binding $project_id --member serviceAccount:$project_number@cloudservices.gserviceaccount.com --role roles/container.admin --quiet
+}
+
+enable_services() {
+	gcloud services enable container.googleapis.com 
+	gcloud services enable iam.googleapis.com
+	gcloud services enable cloudresourcemanager.googleapis.com
+	gcloud services enable runtimeconfig.googleapis.com
+	gcloud services enable sourcerepo.googleapis.com
+	gcloud services enable logging.googleapis.com
+	gcloud services enable monitoring.googleapis.com
+	gcloud services enable apigee.googleapis.com
+	gcloud services enable apigeeconnect.googleapis.com
+}
+
+update() {
+	gcloud deployment-manager deployments update $1 --config apigee-cluster.yaml
+}
+
+add_roles
+enable_services
+update
